5ab9a8275ba91e06f2d415e12b39ef72d64df215
Agywa <91954956+Agywa@users.noreply.github.com>
2021-12-26 13:37:42 +0300

    JS/TS_vanila

---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {splitInterWords} from "./01-hello-tests/01";
+
+const sentense = "Hello my friend!"
+const result = splitInterWords(sentense)
+console.log(result[0] === "hello")
+console.log(result[1] === "my")
+console.log(result[2] === "friend")
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
